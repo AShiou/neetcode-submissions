dd3144cce2b541f1e0c714ed75965048f046ee8a
Wen Hsiu Chang <aa8693@gmail.com>
2026-09-07 21:55:45 +0800

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c: t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            int times = map.get(c);
+            if (times < 1) {
+                return false;
+            }
+            map.put(c, times - 1);
+        }
+        return true;
+    }
+}
