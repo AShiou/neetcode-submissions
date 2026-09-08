eea8ca8c0dfd598108e71c5ff1f9c372ec5e1479
Wen Hsiu Chang <aa8693@gmail.com>
2026-09-08 14:08:52 +0800

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
+    }
+
+    private int robRange(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start + 1], nums[start]);
+        for (int i = start + 2; i <= end ; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end];
+    }
+}
