48d605c4b9d515f1919e21c2dd0e09d5d159e338
Wen Hsiu Chang <aa8693@gmail.com>
2026-09-14 14:32:45 +0800

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int end = 0;
+        int farest = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            farest = Math.max(farest, i + nums[i]);
+
+            if (i == end) {
+                jumps++;
+                end = farest;
+            }
+        }
+        return jumps;
+    }
+}
